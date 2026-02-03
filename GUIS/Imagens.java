52042c5ba38d9f38bad7b6b3ca170d58ae41f82a
Caio Dias <caiodevgit@gmail.com>
2026-02-03 10:02:22 -0200

    mais uma parte de guis java

--- GUIS/Imagens.java
+++ GUIS/Imagens.java
@@ -1,12 +1,21 @@
 package GUIS;
 
+import javax.swing.JFrame;
+
 public class Imagens 
 {
     public static void main(String[] args)
     {
         Caixa caixa = new Caixa();
+        DrawPanel draw = new DrawPanel();
 
         caixa.box();
         caixa.userBox();
+
+        JFrame application = new JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(draw);
+        application.setSize(250,250);
+        application.setVisible(true);
     }
 }
