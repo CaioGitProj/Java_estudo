40f67dfb8c2972df8932312b40fbc9f0d15b52fe
Caio Dias <caiodevgit@gmail.com>
2026-02-03 21:27:33 -0200

    funções Math do java

---
+++ Mat/Matematica.java
@@ -0,0 +1,29 @@
+package Mat;
+
+public class Matematica 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Math.abs(23.7));// módulo do número
+
+        System.out.println(Math.ceil(3.7));//número arrendondado para cima
+
+        System.out.println(Math.exp(3));//exponencial
+
+        System.out.println(Math.floor(3.7));//número arrendondado para baixo
+
+        System.out.println(Math.log(17));//logarítmo
+
+        System.out.println(Math.max(100.3, 2.4)); //maior número entre dois
+
+        System.out.println(Math.min(100.3, 2.4));// menor número entre dois
+
+        System.out.println(Math.pow(3,2));// número (x) elevado a potência (y)
+
+        System.out.println(Math.sin(2));// Seno do ângulo de número x
+
+        System.out.println(Math.sqrt(25));// Raiz quadrada ne x
+
+        System.out.println(Math.tan(2));// tangente do ângulo de número x
+    }
+}
