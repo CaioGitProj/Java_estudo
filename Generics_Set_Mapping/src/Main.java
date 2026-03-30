c316f0d6033b0f1523496cb435bde9819da1bf5f
Caio Dias <caiodevgit@gmail.com>
2026-03-30 20:52:53 -0300

    generics delimitados

--- Generics_Set_Mapping/src/Main.java
+++ Generics_Set_Mapping/src/Main.java
@@ -1,5 +1,12 @@
+import entities.Product;
+import services.CalculationService;
 import services.PrintService;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main
@@ -26,6 +33,25 @@ public class Main
 
          */
 
+        List<Product> list = new ArrayList<>();
+        String path = "/home/pc/Documentos/Java_Projects/in.txt";
 
+        try(BufferedReader br = new BufferedReader(new FileReader(path)))
+        {
+            String line = br.readLine();
+            while(line != null)
+            {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            IO.println("Max:");
+            IO.println(x);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
