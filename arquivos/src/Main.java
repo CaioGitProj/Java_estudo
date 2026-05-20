ecb25a6e16ef58570353c2f738a4e963d49d1450
Caio Dias <caiodevgit@gmail.com>
2026-05-20 12:05:23 -0300

    Lendo arquivos

---
+++ arquivos/src/Main.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        /*
+        File file = new File("c:\\home\\pc\\Documentos\\Java_Projects\\good.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine())
+            {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        finally{
+            if(sc != null)
+            {
+                sc.close();
+            }
+        }
+         */
+        /*
+        String path = "/home/pc/Documentos/Java_Projects/good.txt";
+
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path)))
+        {
+            String line = br.readLine();
+            while(line != null)
+            {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Erro: " + e.getMessage());
+        }
+         */
+
+
+    }
+}
